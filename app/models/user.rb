e7f7b119c188752433787000bae6cea62c3b17b2
栗田大輔 <daikons.msn36@gmail.com>
2021-08-19 01:06:52 +0900

    Userモデルにバリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   mount_uploader :profile_image, ImageUploader
   has_secure_password
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
