35a9d23f455bf0037406dcc2b1bbd94dd2967b00
栗田大輔 <daikons.msn36@gmail.com>
2021-08-19 21:42:32 +0900

    サインアップページでエラー内容が出力されるように実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render :new
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:profile_image, :profile_image_cache, :name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   mount_uploader :profile_image, ImageUploader
   has_secure_password
+  validates :profile_image, presence: true
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   before_validation { email.downcase! }
 end
