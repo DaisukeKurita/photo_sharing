b72678b225a3392be5c55064ef4516ab1c366285
栗田大輔 <daikons.msn36@gmail.com>
2021-08-20 01:59:44 +0900

    ログイン中のユーザー情報が扱えるようにした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      raise
       redirect_to user_path(user.id)
     else
       flash.now[:danger] = "ログインに失敗しました"
