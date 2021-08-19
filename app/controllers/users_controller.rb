abea8fb620360c67d58689e9fba6ddffe0e9f956
栗田大輔 <daikons.msn36@gmail.com>
2021-08-19 23:18:40 +0900

    ユーザー情報の詳細画面を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      r
     else
       render :new
     end
+  end
+  def show
+    @user = User.find(params[:id])
   end
   private
   def user_params
